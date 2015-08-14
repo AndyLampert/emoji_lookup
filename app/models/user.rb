8be752bd7b7f53d8b3a0fda3c088b9a6ab22f14d
Andy Lampert <lamperta@gmail.com>
2015-08-13 22:21:17 -0600

    listing posts now shows username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  validates :email, presence: true
+  validates :username,
+    :presence => true,
+    :uniqueness => {
+      :case_sensitive => false
+    }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
