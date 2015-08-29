f02a2fe933c92b2b9ebd15980efab21a1193b9c4
Andy Lampert <lamperta@gmail.com>
2015-08-28 19:42:47 -0600

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user = User.new
+user.username = 'IAmTheSeedMan'
+user.email = 'seedman@example.com'
+user.encrypted_password = '#$taawktljasktlw4aaglj'
+user.password = 'arstarst'
+user.password_confirmation = 'arstarst'
+user.save!
+
+post = Post.new
+post.entry = 'The Great Seeding'
+post.user_id = 1
+post.save!
+
+comment1 = Comment.new
+comment1.post_id = 1
+comment1.user_id = 1
+comment1.body = 'Fear the seeded man! All shall peril and wilt!'
+comment1.save!
